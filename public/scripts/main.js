c5c80bb8077d97ef836814c283ef4089d05c7433
Brandon <brandontkessler@gmail.com>
2018-04-24 08:00:18 -0700

    Made entire div clickable on Home

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,3 +3,7 @@ $(".package-list").on("click", function(){
 	$(this).children(".package-dropdown").slideToggle();
 });
 
+// MAKE ENTIRE DIV ON HOMEPAGE CLICKABLE
+$(".item").on("click", function(){
+	window.location.href = $(this).find("a").attr("href");
+});
