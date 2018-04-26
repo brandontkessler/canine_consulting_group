4e8e9281c74ba4bc9e177c57abf3398021ede285
Brandon <brandontkessler@gmail.com>
2018-04-25 21:39:32 -0700

    pet-profile stylings

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,3 +7,13 @@ $(".package-list").on("click", function(){
 $(".item").on("click", function(){
 	window.location.href = $(this).find("a").attr("href");
 });
+
+// MAKE HEIGHT OF PET PROFILE IMAGE = WIDTH
+let imgWidth = $(".pet-profile-pic").width();
+$(".pet-profile-pic").css({'height': imgWidth+'px'});
+
+// RESIZE IMAGE ON WINDOW RESIZE
+$(window).resize(function() {
+	let imgWidth = $(".pet-profile-pic").width();
+	$(".pet-profile-pic").css({'height': imgWidth+'px'});
+});
