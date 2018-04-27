a967c8728882fb6a8284b935d14ef893359950d5
Brandon <brandontkessler@gmail.com>
2018-04-27 15:23:21 -0700

    Added dog object + food pages

--- routes/index.js
+++ routes/index.js
@@ -51,7 +51,34 @@ router.get("/logout", function(req, res){
 
 // Cub School
 router.get("/cub-school", function(req, res){
-	res.render("cub-school");
+	res.render("home-categories/cub-school");
+});
+
+// Health and Wellness
+
+// Food
+router.get("/food", function(req, res){
+	res.render("home-categories/food")
+});
+
+router.get("/food/dry", function(req, res){
+	res.render("home-categories/food/dry");
+});
+
+router.get("/food/wet", function(req, res){
+	res.render("home-categories/food/wet");
+});
+
+router.get("/food/raw", function(req, res){
+	res.render("home-categories/food/raw");
+});
+
+router.get("/food/freeze-dried", function(req, res){
+	res.render("home-categories/food/freeze-dried");
+});
+
+router.get("/food/treats", function(req, res){
+	res.render("home-categories/food/treats");
 });
 
 
